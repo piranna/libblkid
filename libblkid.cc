e48a3a9ffa74a09b530ac2128e86aa04e1ee1b8f
Jesús Leganés-Combarro 'piranna <piranna@gmail.com>
2016-12-29 20:07:03 +0100

    `evaluate_spec()` function

--- libblkid.cc
+++ libblkid.cc
@@ -22,7 +22,7 @@ void evaluate_tag(const Nan::FunctionCallbackInfo<v8::Value>& info)
     return Nan::ThrowTypeError("Token must be a string");
 
   if(info.Length() > 1 && !info[1]->IsString())
-    return Nan::ThrowTypeError("Token must be a string or undefined");
+    return Nan::ThrowTypeError("Value must be a string or undefined");
 
   // Extract arguments data
   Nan::Utf8String token(info[0]);
@@ -38,11 +38,45 @@ void evaluate_tag(const Nan::FunctionCallbackInfo<v8::Value>& info)
   info.GetReturnValue().Set(result);
 }
 
+/**
+ * All returned paths are canonicalized, device-mapper paths are converted to
+ * the /dev/mapper/name format.
+ *
+ * @param {string} spec
+ *        unparsed tag (e.g. "LABEL=foo") or path (e.g. /dev/dm-0)
+ *
+ * @returns {string}
+ *          allocated string with a device name.
+ */
+void evaluate_spec(const Nan::FunctionCallbackInfo<v8::Value>& info)
+{
+  // Check arguments
+  if(info.Length() < 1)
+    return Nan::ThrowTypeError("Spec is missing");
+
+  if(!info[0]->IsString())
+    return Nan::ThrowTypeError("Spec must be a string");
+
+  // Extract arguments data
+  Nan::Utf8String spec(info[0]);
+
+  // Get the device path
+  char* device = blkid_evaluate_spec(*spec, NULL);
+  if(device == NULL)
+    return Nan::ThrowTypeError("Device not found");
+
+  // Return the device path
+  v8::Local<v8::String> result = Nan::New(device).ToLocalChecked();
+  info.GetReturnValue().Set(result);
+}
+
 
 void Init(v8::Local<v8::Object> exports)
 {
   exports->Set(Nan::New("evaluate_tag").ToLocalChecked(),
                Nan::New<v8::FunctionTemplate>(evaluate_tag)->GetFunction());
+  exports->Set(Nan::New("evaluate_spec").ToLocalChecked(),
+               Nan::New<v8::FunctionTemplate>(evaluate_spec)->GetFunction());
 }
 
 NODE_MODULE(libblkid, Init)
--- test.js
+++ test.js
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
 
-const evaluate_tag = require('.').evaluate_tag
+const libblkid = require('.')
 
 
-console.log(evaluate_tag('UUID', '4aef932b-ab17-471f-8e80-4901d96495d9'))
+console.log(libblkid.evaluate_tag('UUID', '4aef932b-ab17-471f-8e80-4901d96495d9'))
+//console.log(libblkid.evaluate_tag('UUID=4aef932b-ab17-471f-8e80-4901d96495d9'))
+
+console.log(libblkid.evaluate_spec('UUID=4aef932b-ab17-471f-8e80-4901d96495d9'))
+console.log(libblkid.evaluate_spec('/dev/sda2'))
